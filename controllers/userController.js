a44104738cb7ddebf5e662e4c414925f8762e728
Jay Momnang <jaymomnang@users.noreply.github.com>
2018-04-13 20:39:35 +0100

    login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
     User = mongoose.model('Users');
 const secret = '#T0m4t03s&V3ggi3s';
 
-var getHash = function(pwd) {
+var getHash = function (pwd) {
     const hash = crypt.createHmac('sha256', secret)
         .update(pwd)
         .digest('hex');
@@ -12,70 +12,70 @@ var getHash = function(pwd) {
 }
 
 
-exports.get_users = function(req, res) {
-    User.find({}, 'email phoneno firstname lastname role isActive', function(err, user) {
+exports.get_users = function (req, res) {
+    User.find({}, 'email phoneno firstname lastname role isActive', function (err, user) {
         if (err)
             res.send(err);
         res.json(user);
     });
 };
 
-exports.get_user = function(req, res) {
-    User.find({ email: req.params.email }, 'email phoneno firstname lastname role isActive', function(err, user) {
+exports.get_user = function (req, res) {
+    User.find({ email: req.params.email }, 'email phoneno firstname lastname role isActive', function (err, user) {
         if (err)
             res.send(err);
         res.json(user);
     });
 };
 
-exports.add_new_user = function(req, res) {
+exports.add_new_user = function (req, res) {
     var new_user = new User(req.body);
     var pwd = getHash(new_user.pwd);
     new_user.pwd = pwd;
 
     var obj = helpers.saveData(new_user);
-    obj.then(function(result){
+    obj.then(function (result) {
         return result;
-    });   
-   
+    });
+
 };
 
-exports.authenticateViaEmail = function(req, res) {
-    var password = getHash(req.params.pwd);    
-    User.find({ email: req.params.email, pwd: password }, function(err, user) {
+exports.authenticateViaEmail = function (req, res) {
+    var password = getHash(req.params.pwd);
+    User.find({ email: req.params.email, pwd: password }, function (err, user) {
         if (err)
             res.send(err);
         res.json(user);
     });
 };
 
-exports.authenticateViaPhone = function(req, res) {
-    var password = getHash(req.params.pwd);    
-    User.find({ phoneno: req.params.phoneno, pwd: password }, function(err, user) {
+exports.authenticateViaPhone = function (req, res) {
+    var password = getHash(req.params.pwd);
+    User.find({ phoneno: req.params.phoneno, pwd: password }, function (err, user) {
         if (err)
             res.send(err);
         res.json(user);
     });
 };
 
-exports.update_user_prof = function(req, res) {
-    User.findOneAndUpdate({ email: req.params.email }, req.body, { new: true }, function(err, user) {
+exports.update_user_prof = function (req, res) {
+    User.findOneAndUpdate({ email: req.params.email }, req.body, { new: true }, function (err, user) {
         if (err)
             res.send(err);
         res.json(user);
     });
 };
 
-exports.delete_user = function(req, res) {
-    User.findOneAndUpdate({ email: req.params.email, status: 'suspended', isActive: false }, req.body, { new: true }, function(err, user) {
+exports.delete_user = function (req, res) {
+    User.findOneAndUpdate({ email: req.params.email, status: 'suspended', isActive: false }, req.body, { new: true }, function (err, user) {
         if (err)
             res.send(err);
         res.json({ message: 'User deactivated successfully' });
     });
 };
 
-exports.removeUser = function(req, res) {
-    User.remove({ email: req.params.email }, function(err, user) {
+exports.removeUser = function (req, res) {
+    User.remove({ email: req.params.email }, function (err, user) {
         if (err)
             res.send(err);
         res.json({ message: 'User successfully removed' });
